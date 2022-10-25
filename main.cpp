fa006f4952221b07bd2d62bd8d5fd7ff03d7df88
Eason Lu <github@easonlu.ml>
2022-10-25 18:16:30 +0200

    update

--- main.cpp
+++ main.cpp
@@ -12,25 +12,24 @@ int binaryToDecimal(int n)
 {
     int num = n;
     int dec_value = 0;
- 
-    // Initializing base value to 1, i.e 2^0
     int base = 1;
- 
     int temp = num;
     while (temp) {
         int last_digit = temp % 10;
         temp = temp / 10;
- 
         dec_value += last_digit * base;
- 
         base = base * 2;
     }
- 
     return dec_value;
 }
     int main () {
         using namespace std::this_thread;
         using namespace std::chrono;
+        while(true){
+        cout <<"Do you want to convert from binary to decimal or decimal to binary?"<<endl<<"Please type 1 for binary to decimal,"<<endl<<"Please type 2 for decimal to binary,"<<endl<<"Please imput:";
+        int decbin;
+        cin >> decbin;
+        if (decbin == 1){
         int num;
         while (true){
         cout << "what is your binary number: ";
@@ -55,5 +54,16 @@ int binaryToDecimal(int n)
         }
         }
         cout <<"Your number is : "<< binaryToDecimal(num) << endl;
+        cout <<"Your program returned with exit code of 0"<<endl<<"Please press run to run the program again.";
+        break;
+        }
+        else if (decbin==1){
+        cout <<"Your program returned with exit code of 0"<<endl<<"Please press run to run the program again.";
+        break;
+        }
+        else{
+            cout<<endl<< "Invalid selection."<<endl;
+        }
+        }
         return 0;
     }
